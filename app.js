93226916bc0a07150e67105414e0add63a0e6348
Shakti <shaktisarkar1999@gmail.com>
2021-11-21 14:32:11 +0530

    added start script

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const helmet = require("helmet");
 const MongoStore = require("connect-mongo");
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelpCamp";
 // "mongodb://localhost:27017/yelpCamp"
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
     .connect(dbUrl, {
